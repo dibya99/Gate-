6648d6df367b9bd87cafcdf73386a101519f6e67
dibya99 <pupul31051999@gmail.com>
2019-06-17 22:52:56 +0530

    1st commit

---
+++ src/CMakeFiles/gd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gd.dir/gatedetect.cpp.o"
+  "gd.pdb"
+  "gd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
